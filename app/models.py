908d34f6a244f89250e84aae3dfa045b53c975e1
Karolis Markevičius <karolis.markevicius@protonmail.ch>
2019-05-25 15:48:56 +0200

    account confirmation

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from . import db, login_manager
+from . import login_manager, db
 import uuid as uuid_lib
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
